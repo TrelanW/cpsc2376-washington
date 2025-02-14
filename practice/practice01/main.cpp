30e1c08afc0332998876ecc21275ca1bd0a78a33
TrelanW <tawashington@ualr.edu>
2025-02-13 23:02:32 -0600

    Added function to calculate sum and to find the multiple of a user inputted number.

--- practice/practice01/main.cpp
+++ practice/practice01/main.cpp
@@ -29,18 +29,46 @@ void doubleVector(std::vector<int>& numbers) {
     else {
         for (int& num : numbers) {
             num *= 2;
-        }
-        /*for (int i = 0; i < numbers.size(); ++i) {
-            std::cout << numbers.at(i) * 2 << std::endl;*/
-        
+        }  
     }
+}
+
+void findMultiple(const std::vector<int>& numbers) {
+    int multiple;
+    std::cout << "For what number would like to find multiples of: ";
+    std::cin >> multiple;
+
+    if (multiple == 0) {
+        std::cout << "Cannot check for multiples of zero!" << std::endl;
+        return;
+    }
+    else {
+        bool found = false;
+        for (int num : numbers) {
+            if (num % multiple == 0) {
+                std::cout << num << " ";
+                found = true;
+            }
+            else if (!found) {
+                std::cout << "None found." << std::endl;
+            }
+        }
+    }
+}
+
+// Function to calculate sum
+int getSum(const std::vector<int>& numbers) {
+    int sum = 0;
+    for (int num : numbers) {
+        sum += num;
+    }
+    return sum;
 }
 
 int main()
 {
     std::vector<int> numbers;
     int choice = 0;
-    int num;
 
     //Displays the menu and asks for the user input
     while (choice != 6) {
@@ -49,7 +77,7 @@ int main()
         std::cout << "2. Print vector" << std::endl;
         std::cout << "3. Double the vector" << std::endl;
         std::cout << "4. Find the sum" << std::endl;
-        std::cout << "5. Print Multiples" << std::endl;
+        std::cout << "5. Find Multiples" << std::endl;
         std::cout << "6. Exit" << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
@@ -70,10 +98,10 @@ int main()
             std::cout << "Doubled the vector!!" << std::endl;
             }
         else if (choice == 4) {
-            std::cout << "Found the sum!!" << std::endl;
+            std::cout << "Sum: " << getSum(numbers) << std::endl;
             }
         else if (choice == 5) {
-            std::cout << "The multiples are..." << std::endl;
+            findMultiple(numbers);
             }
         else if (choice != 6) {
             std::cout << "Invalid Choice!! Please try again!" << std::endl;
